d37b1977f36dee66097e1b16887a16c9b6838d9f
tococo852 <tococo852@gmail.com>
2019-09-07 01:29:33 -0400

    funcion anterior modularizada, ninguna nueva funcionalidad

--- Tarea1.c
+++ Tarea1.c
@@ -37,6 +37,57 @@ struct stat st = {0};
 /*
 CreateFolder
 inputs:
+	name= nombre de la que se quiere la direccion
+	addSlash= agrega una slash extra para usar la direccion directamente a la hora de crear archivos 
+Funcion:
+	crea una direccion a una carpeta
+Retorno:
+	direction= un string con la direcion
+
+ejemplos:
+	si name=mazo y addSlash=1 retorna "sourcePath/mazo/"
+	si name=mazo y addSlash=0 retorna "sourcePath/mazo"
+
+*/
+
+
+char * GetDirection(char *name, int addSlash){
+	//get path
+	char cwd[256];
+	getcwd(cwd, sizeof(cwd));
+
+	//add / to the name
+	char sname[strlen(name)];
+	char *slash= "/";
+	snprintf(sname, strlen(name)+ strlen(slash)+1, "%s%s", slash, name);
+
+
+	//merge the strings
+	char outnam[strlen(sname)+strlen(cwd)];
+	snprintf(outnam, strlen(sname)+strlen(cwd)+1, "%s%s", cwd, sname);
+
+	if(addSlash==1){
+
+		//merge the strings
+		char *direction = malloc (sizeof (char) * (strlen(sname)+strlen(cwd) +1));
+		snprintf(direction, strlen(sname)+strlen(cwd)+2, "%s%s%s", cwd, sname,slash);
+
+		return direction;
+
+	}
+
+	else{//addSlash==0
+		//merge the strings
+		char *direction = (char *) malloc (sizeof (char) * (strlen(sname)+strlen(cwd)));
+		snprintf(direction, strlen(sname)+strlen(cwd)+1, "%s%s", cwd, sname);
+		return direction;
+
+	}
+}
+
+/*
+CreateFolder
+inputs:
 	name= nombre de la carpeta que se quiere crear
 Funcion:
 	crea una carpeta en el directorio actual con el nombre name
@@ -44,37 +95,33 @@ Retorno:
 	no retorna nada
 */
 
+
 void CreateFolder(char *name) {
-
-	//get path
-	char cwd[256];
-	getcwd(cwd, sizeof(cwd));
-	printf("cwd = %s\n",cwd );
-
-	//add / to the name
-	char sname[strlen(name)];
-	char *slash= "/";
-	snprintf(sname, strlen(name)+ strlen(slash)+1, "%s%s", slash, name);
-
-	printf("name= %s,sname= %s, largo %li\n",name,sname , strlen(name)+ strlen(slash));
-
-	//merge the strings
-	char outnam[strlen(sname)+strlen(cwd)];
-	snprintf(outnam, strlen(sname)+strlen(cwd)+1, "%s%s", cwd, sname);
-	printf("out name= %s\n",outnam );
-
+	char *outnam= GetDirection(name, 0);
+	
 	//create the folder if it doesnt exist
 	if (stat(outnam, &st) == -1){
-		mkdir(outnam, 0700);}
+		mkdir(outnam, 0700);
+	}
+
+	free(outnam);
+
 }
 
 
 
 int main() {
+
+	//obtiene el dir actual
+
+	
 	char *name2;
 	//nombre del folder
 	name2="mazo";
 	CreateFolder(name2);
+
+	char* direct1= GetDirection("mazo",0);
+	printf("%s \n",direct1 );
 
    
 	return 0;
